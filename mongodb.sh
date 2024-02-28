4b96d0f6286fa1a162b533d4e59344efa599d40a
Rahul-Akki <rahul.akireddy@gmail.com>
2024-02-27 23:37:58 -0500

    roboshop shell script

---
+++ mongodb.sh
@@ -0,0 +1,35 @@
+#!bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "scrip started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2...$R FAILED $N"
+    else
+        echo -e "$2...$R SUCCESS $N"
+    fi
+}
+
+
+ID=$(id -u) #--> Sudo access validation
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR : Please run this script with root access $N" 
+    exit 1 # we can can give any number other than 0. -->   # EXIT STATUS --> echo $? = 0
+else
+    echo "Your are root user"
+fi # fi indicates, end of if condition
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo $>> $LOGFILE
+VALIDATE $? "Copy MongoDP Repo"
+
