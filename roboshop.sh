d83202d708af6c86624eae28dc8cd53dccf35f55
Rahul-Akki <rahul.akireddy@gmail.com>
2024-03-01 21:17:56 -0500

    shell-script

---
+++ roboshop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+AMI=ami-0f3c7d07486cad139
+SG_ID=sg-06b9f4678b7c43359
+INSTANCE=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispach" "web")
+
+for i in "${INSTANCES[@]}"
+do
+    echo "Instance is $i"
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.small"
+    fi
+
+    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+
+done
