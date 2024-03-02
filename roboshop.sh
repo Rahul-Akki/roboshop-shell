4e8fefb4492679cb4f17e9442ba70c379d6a15a1
Rahul-Akki <rahul.akireddy@gmail.com>
2024-03-01 21:23:54 -0500

    shell-script

--- roboshop.sh
+++ roboshop.sh
@@ -6,14 +6,14 @@ INSTANCE=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shippi
 
 for i in "${INSTANCES[@]}"
 do
-    echo "Instance is $i"
+    echo "Instance is: $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
     else
-        INSTANCE_TYPE="t2.small"
+        INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-06b9f4678b7c43359 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 
 done
