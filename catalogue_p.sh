b76f99cf9a2d947e9a6946d54282a3b9e7f015bd
Rahul-Akki <rahul.akireddy@gmail.com>
2024-08-25 20:54:10 -0400

    Edited

---
+++ catalogue_p.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP="date +%m-%d-%Y_%H=%M-%S"
+LOGFILE="/tmp/$0-$TIMESTAMP.log" 
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 $R Failed..! $N "
+        exit 1
+    else 
+        echo -e "$2 $G succeeded.. $N "
+    fi
+}
+
+ID=$( id -u )
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R You are not the root User, This Package Need root access for installation $N"
+    exit1    
+else
+    echo -e "$G You are the root User... $N"
+fi
