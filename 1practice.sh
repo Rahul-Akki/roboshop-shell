105b8d37bc84167928dfca626e38b20a8fc14936
Rahul-Akki <rahul.akireddy@gmail.com>
2024-08-25 20:29:43 -0400

    Edited

--- 1practice.sh
+++ 1practice.sh
@@ -5,6 +5,9 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP="date +%m-%d-%Y_%H=%M-%S"
+LOGFILE="/tmp/$0-$TIMESTAMP.log" 
+
 VALIDATE() {
     if [ $1 -ne 0 ]
     then
@@ -25,15 +28,21 @@ else
     echo -e "$G You are the root User... $N"
 fi
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo #Setup the MongoDB repo file
+cp mongo.repo /etc/yum.repos.d/mongo.repo  &>> $LOGFILE #Setup the MongoDB repo file
 VALIDATE $? "MongoDB repo file setup is"
 
-dnf install mongodb-org -y #Install MongoDB
+dnf install mongodb-org -y  &>> $LOGFILE #Install MongoDB
 VALIDATE $? "Installation of MongoDB is"
 
-systemctl enable mongod 
+systemctl enable mongod  &>> $LOGFILE
 VALIDATE $? "Enableing of MongoDB Service" 
 
-systemctl start mongod 
+systemctl start mongod  &>> $LOGFILE
 VALIDATE $? "Starting of MongoDB Service" 
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf  &>> $LOGFILE
+VALIDATE $? "Remote access to MongoDB"
+
+systemctl restart mongod &>> $LOGFILE
+VALIDATE $? "Restarting MongoDB"
 
